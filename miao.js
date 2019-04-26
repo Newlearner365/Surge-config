a520df3a572f35be37c6fb5f8a9b4cf4de793c92
Newlearner365 <42471848+Newlearner365@users.noreply.github.com>
2019-04-26 21:37:23 +0800

    Update miao.js

--- miao.js
+++ miao.js
@@ -16,13 +16,13 @@ function utf8StringToUtf16String(str) {
 }
 
 
-var keywords = ['复联4', '复联', '复仇者联盟4', '复仇者联盟', '钢铁侠', '托尼', '美国队长', '美队', '黑寡妇', '寡姐', '蜘蛛侠', '浩克', '绿巨人', '鹰眼', '灭霸', '雷神', '惊奇队长', '惊奇', '蚁人', '银河护卫队', '银队'];
+var keywords = ['复联4','复联','复仇者联盟4','复仇者联盟','钢铁侠','托尼','美国队长','美队','黑寡妇','寡姐','蜘蛛侠','浩克','绿巨人','鹰眼','灭霸','雷神','惊奇队长','惊奇','蚁人','银河护卫队','银队'];
 
 var result = body;
 
 keywords.forEach(function(k) {
-  result = replaceAll(result, k, '喵喵喵');
-  result = replaceAll(result, utf8StringToUtf16String(k), "\u55b5\u55b5\u55b5");  
+  result = replaceAll(result,k,'喵喵喵');
+  result = replaceAll(result,utf8StringToUtf16String(k), "\u55b5\u55b5\u55b5");  
 });
 
 result;
